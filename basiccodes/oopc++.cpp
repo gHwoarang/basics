318afe7c0c67e503837cbb7380a8f501400694de
gBalina <109747826+gBalina@users.noreply.github.com>
2024-06-29 14:29:48 +0100

    Add files via upload

---
+++ basiccodes/oopc++.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+class nokta {
+
+public :
+int x, y ;
+
+};
+
+
+int main (){
+nokta n;
+n.x=0;
+cout << n.x << endl;
+
+}
---
+++ basiccodes/oopplus.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class Nokta {
+
+public :
+int x, y ;
+
+};
+
+
+int main (){
+Nokta n;
+n.x=0;
+n.y=5;
+cout << n.x << n.y << endl;
+return 0;
+
+
+
+
+
+
+}
